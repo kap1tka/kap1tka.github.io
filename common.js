3f91463c3bcf1125f7c40c61cbf61168d6b2afd1
kap1tka <37198877+kap1tka@users.noreply.github.com>
2018-03-10 02:39:16 +0300

    7

--- common.js
+++ common.js
@@ -2,7 +2,13 @@ $(function() {
 
 	$('.menuToggle').on('click', function(){
 
-			$('.main-list').slideToggle(300);
+			$('.main-list').slideToggle(300, function(){
+
+				if($(this).css('display') === 'none') {
+					$(this).removeAttr('style');
+			}
+
+			});
 
 	});
 
