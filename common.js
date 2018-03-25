4e75f0c1cb4e7bebce1a64046745d75ffcee9a1e
kap1tka <37198877+kap1tka@users.noreply.github.com>
2018-03-25 21:09:42 +0300

    15

---
+++ common.js
@@ -0,0 +1,35 @@
+$(function() {
+
+	$('.menuToggle').on('click', function(){
+
+			$('.main-list').slideToggle(300, function(){
+
+				if($(this).css('display') === 'none') {
+					$(this).removeAttr('style');
+			}
+
+			});
+
+	});
+
+});
+
+$(document).ready(function() {
+	$('#mainHeader').removeClass('mainHeaderFixed');
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 0) {
+			$('#mainHeader').addClass('mainHeaderFixed').fadeIn('fast');
+			$('li a').css('color', '#fff');
+		} else {
+			$('#mainHeader').removeClass('mainHeaderFixed').fadeIn('fast');
+			$('li a').css('color', '#000');
+		};
+	});
+
+});
+
+$(document).ready(function() {
+	$('main').slideUp(0).slideDown(1000)
+
+
+});
